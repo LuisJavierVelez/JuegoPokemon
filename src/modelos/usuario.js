4f56a85a6e334f3ee120e9f0bb2bda330371be4f
Luis Javier Velez Uribe <93560902+LuisJavierVelez@users.noreply.github.com>
2024-08-12 20:43:15 -0500

    Creacion Proyecto

---
+++ src/modelos/usuario.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) =>{
+    return sequelize.define('Usuario', {
+        cedula:{
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNul: false,
+            unique: 'cedula'
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: 'email'
+        },
+        nombre:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        edad:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {
+        tableName: 'usuario',
+        timestamps: true
+
+    });
+};
